295f5f223aae1b4e391bde930f30c8cecc625e98
DeHess <nathhess@gmail.com>
2025-03-21 10:42:15 +0100

    nav works

--- 4chan-wrapper/App.js
+++ 4chan-wrapper/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import BoardOverviewScreen from './BoardOverviewScreen';
 import BoardScreen from './BoardScreen';
 
 const Stack = createStackNavigator();
@@ -9,7 +10,8 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="BoardScreen" component={BoardScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="BoardOverviewScreen" component={BoardOverviewScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="BoardScreen" component={BoardScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- 4chan-wrapper/BoardScreen.js
+++ 4chan-wrapper/BoardScreen.js
@@ -1,28 +1,10 @@
-import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import useFetchBoards from './utilityJs/fetchBoardData';
-import openBoardLink from './utilityJs/openBoardLink';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 const BoardScreen = () => {
-  const navigation = useNavigation();
-  const boards = useFetchBoards();
-
   return (
     <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.header}>4chan Boards</Text>
-      </View>
-      <ScrollView style={styles.scrollView}>
-      {boards.length > 0 ? (
-          boards.map((board, index) => (
-            <TouchableOpacity key={index} style={styles.item} onPress={() => openBoardLink(board.board)}>
-              <Text style={styles.itemText}>{board.title}</Text>
-            </TouchableOpacity>
-          ))
-        ) : (
-          <Text style={styles.loadingText}>Loading...</Text>
-        )}
-      </ScrollView>
+      <Text style={styles.text}>Hello, World!</Text>
     </View>
   );
 };
@@ -30,47 +12,14 @@ const BoardScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  headerContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: '#fff',
-    paddingVertical: 15,
+    justifyContent: 'center',
     alignItems: 'center',
-    elevation: 4,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 2,
-    zIndex: 10,
+    backgroundColor: '#fff',
   },
-  header: {
-    fontSize: 20,
+  text: {
+    fontSize: 24,
     fontWeight: 'bold',
-  },
-  scrollView: {
-    marginTop: 60,
-    paddingHorizontal: 20,
-  },
-  item: {
-    backgroundColor: '#f0f0f0',
-    padding: 15,
-    marginVertical: 5,
-    borderRadius: 8,
-  },
-  itemText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  loadingText: {
-    fontSize: 16,
-    textAlign: 'center',
-    marginTop: 20,
   },
 });
 
 export default BoardScreen;
-
